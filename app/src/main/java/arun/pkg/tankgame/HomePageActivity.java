5b13bdc6ebaf0df6d3460b4ad285c64b30a0cbc4
Arun Kumar <arun0102@gmail.com>
2024-02-07 23:19:27 -0500

    - Removed Google Ads - Updated to latest gradle version

--- app/src/main/java/arun/pkg/tankgame/HomePageActivity.java
+++ app/src/main/java/arun/pkg/tankgame/HomePageActivity.java
@@ -9,7 +9,6 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.google.android.gms.ads.MobileAds;
 
 import arun.pkg.tankgame.constants.Constants;
 
